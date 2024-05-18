db1ab26e595cd8a0584cd198c9f020b30e884807
KahfiSmith <alkahfii2018@gmail.com>
2024-05-18 18:34:00 +0700

    feat:  belajar use effect

--- src/pages/product.jsx
+++ src/pages/product.jsx
@@ -1,7 +1,7 @@
 import Button from "../components/Elements/Button";
 import CardProduct from "../components/Fragments/CardProduct";
-import Counter from "../components/Fragments/Counter";
-import { useState } from "react";
+// import Counter from "../components/Fragments/Counter";
+import { useEffect, useState } from "react";
 
 const products = [
   {
@@ -30,12 +30,24 @@ const products = [
 const email = localStorage.getItem("email");
 
 const ProductPage = () => {
-  const [cart, setCart] = useState([
-    {
-      id: 1,
-      qty: 1,
-    },
-  ]);
+  const [cart, setCart] = useState([]);
+  const [totalPrice, setTotalPrice] = useState(0);
+
+  useEffect(() => {
+    setCart(JSON.parse(localStorage.getItem("cart")) || []);
+  }, [])
+
+  useEffect(() => {
+    if (cart.length > 0) {
+      const sum = cart.reduce((acc, item) => {
+        const product = products.find((product) => product.id === item.id);
+        return acc + product.price * item.qty;
+      }, 0);
+      setTotalPrice(sum);
+      localStorage.setItem("cart", JSON.stringify(cart));
+    }
+  }, [cart]);
+
 
   const handleAddtoCart = (id) => {
     if (cart.find((item) => item.id === id)) {
@@ -117,13 +129,28 @@ const ProductPage = () => {
                   </tr>
                 );
               })}
+              <tr>
+                <td colSpan={3}>
+                  <b>Total Price</b>
+                </td>
+                <td>
+                  <b>
+                    {" "}
+                    Rp{" "}
+                    {totalPrice.toLocaleString("id-ID", {
+                      styles: "currency",
+                      currency: "IDR",
+                    })}
+                  </b>
+                </td>
+              </tr>
             </tbody>
           </table>
         </div>
       </div>
-      <div className="flex justify-center mb-4">
+      {/* <div className="flex justify-center mb-4">
         <Counter />
-      </div>
+      </div> */}
     </>
   );
 };
